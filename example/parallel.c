e719d9320e58dcf0c9c6b921e473f79149425d14
Xin Liang <lx.altria@gmail.com>
2018-03-23 23:37:04 -0500

    change parallel timing

--- example/parallel.c
+++ example/parallel.c
@@ -159,12 +159,12 @@ int main(int argc, char * argv[])
 	if (world_rank == 0)
 	{
 		printf ("SZ Finish parallel compressing.\n");
-		printf ("Timecost of reading original files = %.2f seconds\n", costReadOri/world_size);
-		printf ("Timecost of reading compressed files = %.2f seconds\n", costReadZip/world_size);
-		printf ("Timecost of writing compressed files = %.2f seconds\n", costWriteZip/world_size);
-		printf ("Timecost of writing decompressed files = %.2f seconds\n", costWriteOut/world_size);
-		printf ("Timecost of compressing using %d processes = %.2f seconds\n", world_size, costComp/world_size);
-		printf ("Timecost of decompressing using %d processes = %.2f seconds\n\n", world_size, costDecomp/world_size);
+		printf ("Timecost of reading original files = %.2f seconds\n", costReadOri);
+		printf ("Timecost of reading compressed files = %.2f seconds\n", costReadZip);
+		printf ("Timecost of writing compressed files = %.2f seconds\n", costWriteZip);
+		printf ("Timecost of writing decompressed files = %.2f seconds\n", costWriteOut);
+		printf ("Timecost of compressing using %d processes = %.2f seconds\n", world_size, costComp);
+		printf ("Timecost of decompressing using %d processes = %.2f seconds\n\n", world_size, costDecomp);
 	}
 
 	SZ_Finalize();
