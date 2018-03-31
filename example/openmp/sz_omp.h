292583664ee8cd15bf6084452f065d5443e9729f
Xin Liang <lx.altria@gmail.com>
2018-03-30 19:29:27 -0500

    parallel huffman

--- example/openmp/sz_omp.h
+++ example/openmp/sz_omp.h
@@ -7,3 +7,4 @@ unsigned char * SZ_compress_float_3D_MDQ_openmp(float *oriData, size_t r1, size_
 
 void decompressDataSeries_float_3D_openmp(float** data, size_t r1, size_t r2, size_t r3, unsigned char* comp_data);
 
+void init_openmp(int *s, size_t length, int thread_num);
